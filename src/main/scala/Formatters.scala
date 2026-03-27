88244e6c8b41cd0b38c7956df58f1c5a33699116
Catalina Gavilán <mcgavilanuran@mi.unc.edu.ar>
2026-03-27 14:46:48 +0000

    Modifico Formatter para el nuevo tipo

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -2,9 +2,12 @@
 object Formatters {
 
   // Pure function to format posts from a subscription
-  def formatSubscription(url: String, posts: String): String = {
+  def formatSubscription(url: String, posts: List[FileIO.Post]): String = {
     val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
-    val formattedPosts = posts.take(80)
+    val formattedPosts = posts.map{case (title, selftext, created_utc, date) =>
+                          s"-> $title ($date)"
+    }.mkString("\n")
+
     header + "\n" + formattedPosts
   }
 }
