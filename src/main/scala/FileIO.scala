be5035467f4d3c51eb62ce8d05c78b9cb1205289
Valentin Issidoro <vissidoro@mi.unc.edu.ar>
2026-04-08 02:37:29 +0000

    Puliendo postStats.. Ahora imprime 5 posts

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -93,31 +93,9 @@ object FileIO {
 
   def wordsFreq(posts: List[Post]): List[(String, Int)] = {
     
-    val stopwords = Set("the", "about", "above", "after", "again", "against", "all", "am", "an",
-    "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
-    "before", "being", "below", "between", "both", "but", "by", "can't",
-    "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't",
-    "doing", "don't", "down", "during", "each", "few", "for", "from", "further",
-    "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd",
-    "he'll", "he's", "her", "here", "here's", "hers", "herself", "him",
-    "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if",
-    "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me",
-    "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off",
-    "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves",
-    "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's",
-    "should", "shouldn't", "so", "some", "such", "than", "that", "that's",
-    "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
-    "these", "they", "they'd", "they'll", "re", "they've", "this", "those",
-    "through", "to", "too", "under", "until", "up", "very", "was", "wasn't",
-    "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what",
-    "what's", "when", "when's", "where", "where's", "which", "while", "who",
-    "who's", "whom", "why", "why's", "with", "won't", "would",
-    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours",
-    "yourself", "yourselves")
-    
     val words = posts.map(w => w._3.split("\\W+").groupBy(identity _))
     
-    val filteredWords = words.map(fw => fw.filter { case (word, _) => word.nonEmpty && word.head.isUpper && !stopwords.contains(word.toLowerCase) })
+    val filteredWords = words.map(fw => fw.filter { case (word, _) => word.nonEmpty && word.head.isUpper && !StopWords.stopwords.contains(word.toLowerCase) })
 
     val wordsOcc = filteredWords.flatMap (wo => wo.map {case (word, array) => (word, array.length) }
     .toList
@@ -128,6 +106,7 @@ object FileIO {
   def postStats(post: List[Post], url: String): Unit = {
 
     val score = post.foldLeft(0)((acumulado, p) => acumulado + (p._5 - p._6))
+    val CincoPost = post.take(5).map(p => s"""${url} - ${p._2} - ${p._4}""")
     
     println(s"""
     # Estadisticas de los Subreddits
@@ -135,9 +114,7 @@ object FileIO {
     - Total Score: $score
     - Most repeated word: ${wordsFreq(post).head}
     - Cinco primeros post: 
-      ${url}
-      ${post(0)._2}
-      ${post(0)._4}
+    ${CincoPost.mkString("\n  ")}
     """)
 
   }
